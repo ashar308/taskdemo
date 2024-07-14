7dd8c8039f69dd17ccc54ea12f9caff380e870f3
Aishwary Sharma <137591238+ashar308@users.noreply.github.com>
2024-07-14 00:33:23 -0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
-import React,{useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { AddTaskForm } from "./components/AddTaskForm";
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { Task } from "./components/Task";
 import axios from 'axios';
-import { useState } from "react";
 import { API_URL } from "./utils";
+
 const darkTheme = createTheme({
   palette: {
     mode: 'dark',
@@ -20,32 +20,31 @@ const task = {
 };
 }
 */
+
 export default function App() {
-  const[tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState([]);
+
   const fetchTasks = async () => {
-    try{
-      const {data} = await axios.get(API_URL);
+    try {
+      const { data } = await axios.get(API_URL);
       setTasks(data);
-
-    }catch (err){
+    } catch (err) {
       console.log(err);
-
     }
   };
 
   useEffect(() => {
     fetchTasks();
-  },[]);
-
+  }, []);
 
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
-      <AddTaskForm fetchTasks = {fetchTasks}/>
+      <AddTaskForm fetchTasks={fetchTasks} />
       {tasks.map((task) => (
-        <Task task={task} key={task.id} fetchTasks={fetchTasks}/>
-
-       ))}
+        <Task task={task} key={task.id} fetchTasks={fetchTasks} />
+      ))}
     </ThemeProvider>
   );
 }
+
